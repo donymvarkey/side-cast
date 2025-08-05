3945c7a9f63260235f853bb92194d4d08b9520cc
Dony M Varkey <dony.v@applab.qa>
2025-08-05 12:11:39 +0530

    v0.0.1 Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to Sidecast</h1>
+    </div>
+  );
+};
+
+export default App;
